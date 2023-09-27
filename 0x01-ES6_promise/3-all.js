0465cd50bd942b8549731df483d2772c1728801d
Thecodingyogi <cynthiatonui@gmail.com>
2023-09-27 13:33:39 +0000

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      const { body: photo } = photoResponse;
+      const { firstName, lastName } = userResponse;
+      console.log(`${photo} ${firstName} ${lastName}`);
+    })
+    .catch((error) => {
+      console.log('Signup system offline', error);
+    });
+}
