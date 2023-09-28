ea99fb9b0333caaa52d9196512f566b67829af9e
Thecodingyogi <cynthiatonui@gmail.com>
2023-09-28 15:30:55 +0000

    Corrected

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -3,15 +3,14 @@ export default class HolbertonCourse {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    this._name = name;
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
+    this._length = length;
     if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
     }
-
-    this._name = name;
-    this._length = length;
     this._students = students;
   }
 
